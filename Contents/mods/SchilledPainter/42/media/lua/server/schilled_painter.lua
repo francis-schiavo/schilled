59ea9f7b960ed18440f4b6c0c7446cc1ac65fb1a
Francis Schiavo <francischiavo@gmail.com>
2026-09-06 02:45:37 -0300

    feat: Clean lightbulbs

---
+++ Contents/mods/SchilledPainter/42/media/lua/server/schilled_painter.lua
@@ -0,0 +1,10 @@
+SchilledPainter = {}
+SchilledPainter.OnTest = {}
+
+SchilledPainter.OnTest.LightBulbColor = function(item, player)
+    if item:getType() ~= "LightBulb" then
+        return true
+    end
+
+    return item:getColor() ~= Color.white
+end
