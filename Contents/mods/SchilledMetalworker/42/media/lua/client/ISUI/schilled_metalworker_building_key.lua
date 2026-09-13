b97e91dc9decc2ab80de33799fb187824c8d47c7
Francis Schiavo <francischiavo@gmail.com>
2026-09-13 03:31:29 -0300

    fix: Better key creation

--- Contents/mods/SchilledMetalworker/42/media/lua/client/ISUI/schilled_metalworker_building_key.lua
+++ Contents/mods/SchilledMetalworker/42/media/lua/client/ISUI/schilled_metalworker_building_key.lua
@@ -1,21 +1,32 @@
 require("ISUI/ISWorldObjectContextMenu")
 
-local function createDoorKey(player, square)
+local originalWorldContextMenu = ISWorldObjectContextMenu.createMenu;
+
+local function createBuildingKey(player)
     sendClientCommand(player, 'SchilledMetalworker', 'getBuildingKey', {})
 end
 
-local originalWorldContextMenu = ISWorldObjectContextMenu.createMenu;
+local function createDoorKey(player, door)
+    local args = { x = door:getX(), y = door:getY(), z = door:getZ(), index = door:getObjectIndex() }
+    sendClientCommand(player, 'SchilledMetalworker', 'getDoorKey', args)
+end
 
 ISWorldObjectContextMenu.createMenu = function(player, worldobjects, x, y, test)
     local context = originalWorldContextMenu(player, worldobjects, x, y, test);
-
     local playerObj = getSpecificPlayer(player);
-
     local square = playerObj:getCurrentSquare();
-    local recipeKnown = playerObj:isRecipeKnown("CreateBuildingKey", true);
-    if square and square:getBuilding() and recipeKnown then
-        local createKeyOption = context:addOption(getText("ContextMenu_CreateBuildingKey"), playerObj, createDoorKey, square);
+
+    if square and square:getBuilding() and playerObj:isRecipeKnown("CreateBuildingKey", true) then
+        local createKeyOption = context:addOption(getText("ContextMenu_CreateBuildingKey"), playerObj, createBuildingKey);
         createKeyOption.toolTip, createKeyOption.notAvailable = ContextMenuBuilder:CreateMenuTooltip(playerObj, { ["Base.ScrapMetal"] = 5 }, { ["Mechanics"] = 2, ["MetalWelding"] = 2 });
     end
+
+    for _,obj in ipairs(worldobjects) do
+        if instanceof(obj, "IsoDoor") or (instanceof(obj, "IsoThumpable") and obj:isDoor()) and playerObj:isRecipeKnown("CreateDoorKey", true) then
+            local createKeyOption = context:addOption(getText("ContextMenu_CreateDoorKey"), playerObj, createDoorKey, obj);
+            createKeyOption.toolTip, createKeyOption.notAvailable = ContextMenuBuilder:CreateMenuTooltip(playerObj, { ["Base.ScrapMetal"] = 5 }, { ["Mechanics"] = 2, ["MetalWelding"] = 2 });
+        end
+    end
+
     return context;
 end
--- Contents/mods/SchilledMetalworker/42/media/lua/server/schilled_metalworker_server_commands.lua
+++ Contents/mods/SchilledMetalworker/42/media/lua/server/schilled_metalworker_server_commands.lua
@@ -33,6 +33,51 @@ function Commands:getBuildingKey(player)
     end
 end
 
+function Commands:getDoorKey(player, args)
+    local gs = getCell():getGridSquare(args.x, args.y, args.z)
+    if not gs then
+        print('square is nil')
+        return
+    end
+
+    if args.index < 0 or args.index >= gs:getObjects():size() then
+        print('invalid object')
+    end
+    local door = gs:getObjects():get(args.index)
+
+    local keyID = -1
+    if instanceof(door, "IsoDoor") then
+        keyID = door:checkKeyId()
+    elseif instanceof(door, "IsoThumpable") then
+        keyID = door:getKeyId()
+    end
+
+    if keyID == -1 then
+        keyID = ZombRand(100000000)
+    end
+    door:setKeyId(keyID)
+
+    local doubleDoorObjects = buildUtil.getDoubleDoorObjects(door)
+    for i=1,#doubleDoorObjects do
+        local object = doubleDoorObjects[i]
+        object:setKeyId(keyID)
+    end
+
+    local garageDoorObjects = buildUtil.getGarageDoorObjects(door)
+    for i=1,#garageDoorObjects do
+        local object = garageDoorObjects[i]
+        object:setKeyId(keyID)
+    end
+
+    local key = instanceItem("Base.Key1")
+    key:setKeyId(keyID)
+
+    player:getInventory():AddItem(key)
+    sendAddItemToContainer(player:getInventory(), key);
+    local items = player:getInventory():RemoveAll('Base.ScrapMetal', 5)
+    sendRemoveItemsFromContainer(player:getInventory(), items);
+end
+
 local function onClientCommand(module, command, player, args)
     if module ~= 'SchilledMetalworker' then
         return
@@ -42,6 +87,8 @@ local function onClientCommand(module, command, player, args)
         Commands:getBuildingKey(player)
     elseif command == "getVehicleKey" then
         Commands:getVehicleKey(player, args.vehicleId)
+    elseif command == "getDoorKey" then
+            Commands:getDoorKey(player, args)
     end
 end
 
